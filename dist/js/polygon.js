14ae95a4e183904ec8fe99954d60517fd711c1ee
Delveri Chick <chickdelveri@gmail.com>
2018-02-22 11:30:55 -0500

    Saved changes to new structure

---
+++ dist/js/polygon.js
@@ -0,0 +1,64 @@
+/**
+ * Polygon class, simple polygon class with method as rotation and
+ * scaling
+ */
+var Polygon = Class.extend({
+
+	/**
+	 * Constructor
+	 *
+	 * @param  {Array<number>} p list of verticies
+	 */
+	init: function(p) {
+		// copy verticies from param array
+		this.points = p.slice(0);
+	},
+
+	rotate: function(theta) {
+		// simplifying computition of 2x2 matrix
+		// for more information see slides in part 1
+		var c = Math.cos(theta);
+		var s = Math.sin(theta);
+
+		// iterate thru each vertex and change position
+		for (var i = 0, len = this.points.length; i < len; i += 2) {
+			var x = this.points[i];
+			var y = this.points[i+1];
+
+			this.points[i] = c*x - s*y;
+			this.points[i+1] = s*x + c*y;
+		}
+	},
+
+
+	scale: function(c) {
+		// ordinary vector multiplication
+		for (var i = 0, len = this.points.length; i < len; i++) {
+			this.points[i] *= c;
+		}
+	},
+
+
+	hasPoint: function(ox, oy, x, y) {
+		var c = false;
+		var p = this.points;
+		var len = p.length;
+
+		// doing magic!
+		for (var i = 0, j = len-2; i < len; i += 2) {
+			var px1 = p[i] + ox;
+			var px2 = p[j] + ox;
+
+			var py1 = p[i+1] + oy;
+			var py2 = p[j+1] + oy;
+
+			if (( py1 > y != py2 > y ) &&
+			    ( x < (px2-px1) * (y-py1) / (py2-py1) + px1 )
+			) {
+				c = !c;
+			}
+			j = i;
+		}
+		return c;
+	}
+});
---
+++ dist/js/state.js
@@ -0,0 +1,20 @@
+
+var State = Class.extend({
+
+	init: function(game) {
+		// create field to state manager
+		this.game = game;
+	},
+
+	handleInputs: function(input) {
+		return void 0;
+	},
+
+	update: function() {
+		return void 0;
+	},
+
+	render: function(ctx) {
+		return void 0;
+	}
+});
