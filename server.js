a583dd655f7770f22ec01212b958a6128f9d954f
delveri <delveri.chick@anthem.com>
2018-02-23 12:55:42 -0500

    Work in progress

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ app.use(express.static('dist'));
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/dist/index.html');
-})
+});
 
 http.listen(process.env.PORT||5000, ()=>{
   console.log('Server running at : ' + 5000);
 });
 
-
 io.on('connection', (socket)=>{
   console.log('A player joinned');
+  console.log(socket.handshake.query);
 });
