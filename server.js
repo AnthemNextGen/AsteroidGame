a21965b8efa26db531b1c05cb519d956f949c42c
delveri <delveri.chick@anthem.com>
2018-02-26 10:02:23 -0500

    Work in progress: Added private rooms to Server

--- server.js
+++ server.js
@@ -11,12 +11,14 @@ app.get('/', function (req, res) {
 http.listen(process.env.PORT||5000, ()=>{
   console.log('Server running at : ' + 5000);
 });
+const game_rooms = [];
 var room;
 io.use((socket, next)=>{
 console.log(socket.handshake.query);
   room = socket.handshake.query.token;
   if(room){
     socket.join(room);
+    game_rooms.push(room);
     return next();
   }else{
     return next() // Join the public namespace
@@ -27,7 +29,9 @@ io.on('connection', (socket)=>{
   console.log('A player joinned');
   if(room){
     console.log('A player joinned room :' + room);
+    console.log(game_rooms);
   }else{
     console.log('A player Joinned the Public Game');
+    console.log(game_rooms);
   }
 });
